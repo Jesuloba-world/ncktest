450ad973cfea0e74308fe1454e0c6bb75bab3705
Jesuloba <jesulobajohn@gmail.com>
2022-02-04 11:02:47 +0100

    start over

--- src/globalStyles.js
+++ src/globalStyles.js
@@ -11,7 +11,7 @@ const GlobalStyle = createGlobalStyle`
         padding: 0;
         margin: 0;
         box-sizing: border-box;
-        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        font-family: 'Open Sans', 'Helvetica Neue', sans-serif;
     }
 `;
 
